6c5346d43ffc2b7314b24281dc5230bbc22fa53c
miyabayt <4364793+miyabayt@users.noreply.github.com>
2017-10-09 03:42:55 +0900

    コード生成プラグインを追加

---
+++ buildSrc/src/main/groovy/com/sample/CodeGenPluginExtension.groovy
@@ -0,0 +1,20 @@
+package com.sample
+
+class CodeGenPluginExtension {
+    String srcDirName = "src/main/java/"
+    String sqlDirName = "src/main/resources/META-INF/"
+    String htmlDirName = "src/main/resources/templates/modules/"
+    String domainProjectName
+    String webProjectName
+
+    String daoPackageName
+    String dtoPackageName
+    String commonDtoPackageName
+    String servicePackageName
+    String commonServicePackageName
+    String exceptionPackageName
+    String webBasePackageName
+    String baseControllerPackageName
+    String controllerPackageName
+    String baseValidatorPackageName
+}
