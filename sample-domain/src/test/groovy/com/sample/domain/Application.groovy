5376f1fbae006e67e6e14c15e2ecdf1ecb736bd7
miyabayt <4364793+miyabayt@users.noreply.github.com>
2017-11-30 14:49:17 +0900

    fix bugs

--- sample-domain/src/test/groovy/com/sample/domain/Application.groovy
+++ sample-domain/src/test/groovy/com/sample/domain/Application.groovy
@@ -1,8 +1,18 @@
 package com.sample.domain
 
 import com.sample.ComponentScanBasePackage
+import com.sample.domain.dto.common.DefaultPageFactoryImpl
+import com.sample.domain.dto.common.PageFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication(scanBasePackageClasses = ComponentScanBasePackage)
+@TestConfiguration
 class Application {
+
+    @Bean
+    PageFactory pageFactory() {
+        return new DefaultPageFactoryImpl()
+    }
 }
