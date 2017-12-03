31614956b1490dbbab55310c9081085dbf1db11b
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2017-12-03 20:46:08 +0900

    refs #7 パスワード暗号化対応

--- sample-web-admin/src/main/java/com/sample/web/admin/ApplicationConfig.java
+++ sample-web-admin/src/main/java/com/sample/web/admin/ApplicationConfig.java
@@ -3,6 +3,9 @@ package com.sample.web.admin;
 import java.util.Arrays;
 import java.util.List;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySources;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,8 @@ import com.sample.web.base.BaseApplicationConfig;
 
 @Configuration
 @EnableCaching // JCacheを有効可する
+@EnableEncryptableProperties //Passwordを暗号化する
+@EncryptablePropertySources({@EncryptablePropertySource("classpath:application-development.yml")})
 public class ApplicationConfig extends BaseApplicationConfig {
 
     @Override
--- sample-web-front/src/main/java/com/sample/web/front/ApplicationConfig.java
+++ sample-web-front/src/main/java/com/sample/web/front/ApplicationConfig.java
@@ -3,6 +3,9 @@ package com.sample.web.front;
 import java.util.Arrays;
 import java.util.List;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySources;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,9 @@ import com.sample.web.base.BaseApplicationConfig;
 
 @Configuration
 @EnableCaching // JCacheを有効化する
+@EnableEncryptableProperties //Passwordを暗号化する
+@EncryptablePropertySources({@EncryptablePropertySource("classpath:application-development.yml"),
+                                @EncryptablePropertySource("classpath:application-staging.yml")})
 public class ApplicationConfig extends BaseApplicationConfig {
 
     @Override
