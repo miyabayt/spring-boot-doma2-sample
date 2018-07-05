182177b7964ab1143fcc3e55172f7279a9da1cd4
miyabayt <4364793+miyabayt@users.noreply.github.com>
2018-07-05 15:16:58 +0900

    桁数が大きい値を送信した時に、セッション格納先が桁あふれする問題の対策

---
+++ sample-web-base/src/main/java/com/sample/web/base/filter/CheckOverflowFilter.java
@@ -0,0 +1,55 @@
+
+package com.sample.web.base.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.Setter;
+import lombok.val;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * リクエストパラメーターの文字数が想定外に多い場合はエラー画面に遷移させる
+ */
+@Slf4j
+public class CheckOverflowFilter extends OncePerRequestFilter {
+
+    @Setter
+    private int maxLength = 20000;
+
+    private static final String ERROR_URL = "/error";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            val parameterNames = request.getParameterNames();
+            while (parameterNames.hasMoreElements()) {
+                val parameterName = parameterNames.nextElement();
+                val value = request.getParameter(parameterName);
+
+                if (value != null && value.length() > maxLength) {
+                    redirectToErrorPage(request, response);
+                    return;
+                }
+            }
+        } catch (Exception e) {
+            log.error("cloud not parse request parameters", e);
+            redirectToErrorPage(request, response);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void redirectToErrorPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        val contextPath = request.getContextPath();
+        response.sendRedirect(contextPath + ERROR_URL);
+    }
+}
