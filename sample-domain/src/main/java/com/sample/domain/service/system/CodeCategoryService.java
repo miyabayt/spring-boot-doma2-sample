13a57551ab8fd4f72abf1cb06593f3516f97c136
miyabayt <4364793+miyabayt@users.noreply.github.com>
2018-08-18 18:12:22 +0900

    コード分類マスタの追加、コード値取得まわりの整理

--- sample-domain/src/main/java/com/sample/domain/service/system/CodeCategoryService.java
+++ sample-domain/src/main/java/com/sample/domain/service/system/CodeCategoryService.java
@@ -24,18 +24,6 @@ public class CodeCategoryService extends BaseTransactionalService {
     CodeCategoryRepository codeCategoryRepository;
 
     /**
-     * コード分類を全件取得します。
-     *
-     * @return
-     */
-    @Transactional(readOnly = true) // 読み取りのみの場合は指定する
-    public Page<CodeCategory> fetchAll() {
-        // ページングを指定する
-        val pageable = Pageable.NO_LIMIT;
-        return codeCategoryRepository.findAll(new CodeCategory(), pageable);
-    }
-
-    /**
      * コード分類を一括取得します。
      *
      * @return
