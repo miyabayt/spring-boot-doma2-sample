3f856a59d09b50b7fef6e6cfdebbe8eadea7838c
miyabayt <4364793+miyabayt@users.noreply.github.com>
2022-07-24 14:49:13 +0900

    spring boot 2.7.x に対応する

--- sample-web-admin/src/main/java/com/sample/web/admin/ApiSecurityConfig.java
+++ sample-web-admin/src/main/java/com/sample/web/admin/ApiSecurityConfig.java
@@ -1,9 +1,50 @@
 package com.sample.web.admin;
 
-import com.sample.web.base.security.BaseApiSecurityConfig;
+import static com.sample.web.base.WebConst.API_BASE_URL;
+
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
-@Order(1)
-public class ApiSecurityConfig extends BaseApiSecurityConfig {}
+public class ApiSecurityConfig {
+
+  @Autowired UserDetailsService userDetailsService;
+
+  @Autowired PasswordEncoder passwordEncoder;
+
+  @Bean
+  @Order(2)
+  public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+    val authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder
+        .userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder);
+    val authenticationManager = authenticationManagerBuilder.build();
+
+    http.antMatcher(API_BASE_URL)
+        // すべてのリクエストに認証をかける
+        .authorizeRequests()
+        .anyRequest()
+        .authenticated()
+        // Basic認証をかける
+        .and()
+        .authenticationManager(authenticationManager)
+        .httpBasic()
+        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        // CSRFチェックをしない
+        .and()
+        .csrf()
+        .disable();
+    return http.build();
+  }
+}
--- sample-web-base/src/main/java/com/sample/web/base/security/BaseSecurityConfig.java
+++ sample-web-base/src/main/java/com/sample/web/base/security/BaseSecurityConfig.java
@@ -5,27 +5,19 @@ import static com.sample.web.base.WebConst.*;
 import com.sample.web.base.security.rememberme.MultiDeviceRememberMeServices;
 import com.sample.web.base.security.rememberme.MultiDeviceTokenRepository;
 import com.sample.web.base.security.rememberme.PurgePersistentLoginTask;
-import com.sample.web.base.util.RequestUtils;
 import javax.sql.DataSource;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /** 基底セキュリティコンフィグ */
-public abstract class BaseSecurityConfig extends WebSecurityConfigurerAdapter {
+public abstract class BaseSecurityConfig {
 
   @Value("${application.security.secureCookie:false}")
   protected Boolean secureCookie;
@@ -75,78 +67,6 @@ public abstract class BaseSecurityConfig extends WebSecurityConfigurerAdapter {
     task.setTokenPurgeSeconds(tokenPurgeSeconds);
     task.setDataSource(dataSource);
     return task;
-  }
-
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    // 静的ファイルへのアクセスは認証をかけない
-    web.ignoring() //
-        .antMatchers(WEBJARS_URL, STATIC_RESOURCES_URL);
-  }
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService) //
-        .passwordEncoder(passwordEncoder());
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    // CookieにCSRFトークンを保存する
-    http.csrf() //
-        .csrfTokenRepository(new CookieCsrfTokenRepository());
-
-    String[] permittedUrls = {
-      LOGIN_TIMEOUT_URL, FORBIDDEN_URL, ERROR_URL, RESET_PASSWORD_URL, CHANGE_PASSWORD_URL
-    };
-
-    http.authorizeRequests()
-        // エラー画面は認証をかけない
-        .antMatchers(permittedUrls)
-        .permitAll()
-        // エラー画面以外は、認証をかける
-        .anyRequest()
-        .authenticated() //
-        .and() //
-        .exceptionHandling() //
-        .authenticationEntryPoint(authenticationEntryPoint()) //
-        .accessDeniedHandler(accessDeniedHandler());
-
-    http.formLogin()
-        // ログイン画面のURL
-        .loginPage(LOGIN_URL)
-        // 認可を処理するURL
-        .loginProcessingUrl(LOGIN_PROCESSING_URL)
-        // ログイン成功時の遷移先
-        .successForwardUrl(LOGIN_SUCCESS_URL)
-        // ログイン失敗時の遷移先
-        .failureUrl(LOGIN_FAILURE_URL)
-        // ログインIDのパラメータ名
-        .usernameParameter("loginId")
-        // パスワードのパラメータ名
-        .passwordParameter("password")
-        .permitAll();
-
-    // ログアウト設定
-    http.logout() //
-        .logoutRequestMatcher(new AntPathRequestMatcher(LOGOUT_URL))
-        // Cookieを破棄する
-        .deleteCookies("SESSION", "JSESSIONID", rememberMeCookieName)
-        // ログアウト画面のURL
-        .logoutUrl(LOGOUT_URL)
-        // ログアウト後の遷移先
-        .logoutSuccessUrl(LOGOUT_SUCCESS_URL)
-        // ajaxの場合は、HTTPステータスを返す
-        .defaultLogoutSuccessHandlerFor(
-            new HttpStatusReturningLogoutSuccessHandler(), RequestUtils::isAjaxRequest)
-        // セッションを破棄する
-        .invalidateHttpSession(true)
-        .permitAll();
-
-    // RememberMe
-    http.rememberMe()
-        .key(REMEMBER_ME_KEY) //
-        .rememberMeServices(multiDeviceRememberMeServices());
   }
 
   @Bean
